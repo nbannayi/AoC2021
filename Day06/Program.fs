07ee7999ceea4258c886868ccc3a19d0548b0fc9
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2021-12-07 14:25:43 +0000

    Day 6+7

---
+++ Day06/Program.fs
@@ -0,0 +1,44 @@
+﻿open AoC2021.Utilities
+
+// Advent of Code 2021 day 6.
+
+[<EntryPoint>]
+let main _argv =
+    let lanternfishSchool =
+        let lanternfishInput =
+            "InputFiles/Day06Input.txt"
+            |> Seq.ofFileLines
+            |> Seq.head            
+        in lanternfishInput.Split ','
+        |> Array.map (int)        
+        
+    let processLanternfishSchool lanternfishSchool n =
+        let processLanternfish lanternfish =        
+            match lanternfish with
+            | 0 -> [|6; 8|]
+            | _ -> [|lanternfish-1|]
+
+        let processLanternfishSchool' lanternfishSchool = 
+            lanternfishSchool
+            |> Array.map (processLanternfish)
+            |> Array.collect (fun s -> s)
+        (lanternfishSchool, [1..n])
+        ||> List.fold (fun lfs i -> processLanternfishSchool' lfs)
+
+    let lanternfishSchool' = processLanternfishSchool lanternfishSchool 80
+
+    printfn "Part 1: result is %d" lanternfishSchool'.Length
+
+    let lanternfishCounts = Array.create 10 0L
+    let noDays = 256
+
+    lanternfishSchool
+    |> Array.iter (fun f -> (lanternfishCounts.[f] <- lanternfishCounts.[f] + 1L))
+
+    [0..(noDays-1)]
+    |> List.iter (fun d ->        
+        let d' = (d + 7) % 9        
+        lanternfishCounts.[d'] <- lanternfishCounts.[d'] + lanternfishCounts.[d % 9])    
+
+    printfn "Part 2: result is %d" (lanternfishCounts |> Array.sum)
+    0
---
+++ Day07/Program.fs
@@ -0,0 +1,35 @@
+﻿open AoC2021.Utilities
+
+// Advent of Code 2021 day 7.
+
+open System
+
+[<EntryPoint>]
+let main _argv =
+    let crabs = 
+        let crabsInput =        
+            "InputFiles/Day07Input.txt"
+            |> Seq.ofFileLines
+            |> Seq.head
+        in crabsInput.Split "," |> Array.map (int)      
+
+    let crabFuelCalculator1 (c1: int) (c2: int) =
+        Math.Abs(c1-c2)
+
+    let crabFuelCalculator2 (c1: int) (c2: int) =
+        [1..Math.Abs(c1-c2)] |> List.sum
+
+    let getCrabFuelRequirements crabs crabFuelCalculator =
+        [|for c1 in crabs |> Array.distinct do
+            for c2 in crabs -> (c1,c2)|]
+        |> Array.map (fun cp -> fst cp, (crabFuelCalculator (fst cp) (snd cp)))
+        |> Array.groupBy (fst)
+        |> Array.map (fun cp -> (fst cp, (snd cp) |> Array.sumBy (snd)))
+        |> Array.sortBy (snd)
+
+    let crabFuelRequirements1 = getCrabFuelRequirements crabs crabFuelCalculator1
+    printfn "Part 1: result is %d" (crabFuelRequirements1 |> Array.head |> snd)
+
+    let crabFuelRequirements2 = getCrabFuelRequirements crabs crabFuelCalculator2
+    printfn "Part 2: result is %d" (crabFuelRequirements2 |> Array.head |> snd)    
+    0
