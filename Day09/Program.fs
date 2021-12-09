1c380979b1a006dfc3b61c6b7c0587ebdddb3ab3
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2021-12-09 21:49:40 +0000

    Day 9

---
+++ Day09/Program.fs
@@ -0,0 +1,54 @@
+﻿open AoC2021.Utilities
+open AoC2021.Domain
+
+// Advent of Code 2021 day 9.
+
+[<EntryPoint>]
+let main _argv =    
+
+    let heightMapInput =        
+        "InputFiles/Day09Input.txt"
+        |> Seq.ofFileLines
+        |> Array.ofSeq
+
+    let heightMap =
+        heightMapInput
+        |> HeightMap.create
+
+    let lowPoints =
+        [for row in [0..heightMap.height-1] do
+            for col in [0..heightMap.width-1] -> (row,col)]
+        |> List.filter (fun p -> heightMap |> HeightMap.isLowPoint p)
+
+    let sumOfRiskPoints =
+        lowPoints
+        |> List.sumBy (fun p -> 1 + heightMap.Grid.[fst p,snd p].Value)
+
+    printfn "Part 1: result is %d" sumOfRiskPoints
+
+    let rec getBasin (points: (int * int) list) heightMap =
+                
+        let points' =
+            points
+            |> List.map (fun p -> heightMap |> HeightMap.getSurroundingPoints p)
+            |> List.collect (fun p -> p)
+            |> List.filter (fun p -> p.Value < 9)
+            |> List.map (fun p-> p.Position)
+            |> List.distinct
+        
+        points |> List.iter (fun p -> heightMap |> HeightMap.setPoint p 10)
+
+        match points' with
+        | [] -> points
+        | _ -> points @ getBasin points' heightMap
+
+    let productOfTop3Basins =
+        lowPoints
+        |> List.map (fun p -> getBasin [p] heightMap)
+        |> List.map (List.length)
+        |> List.sortDescending
+        |> List.take 3
+        |> List.reduce (*)
+    
+    printfn "Part 2: result is %d" productOfTop3Basins
+    0
---
+++ Domain/HeightMap.fs
@@ -0,0 +1,102 @@
+﻿namespace AoC2021.Domain
+
+// Height map module module for AoC 2021 day 9.
+
+open AoC2021.Utilities
+
+type HeightPointType =
+    | TopLeftCorner
+    | TopMiddleEdge
+    | TopRightCorner
+    | MiddleLeftEdge
+    | Middle
+    | MiddleRightEdge
+    | BottomLeftCorner
+    | BottomMiddleEdge
+    | BottomRightCorner
+    | Unknown
+  
+type HeightPoint =
+    {
+        Value    : int 
+        Type     : HeightPointType
+        Position : int * int 
+    }
+
+    static member init =
+        {
+            Value = 0
+            Type = Unknown
+            Position = (0,0)
+        }
+
+    member this.fill n =
+        {
+            this with Value = n
+        }
+
+type HeightMap =
+    {
+        Grid : HeightPoint [,]      
+    }
+
+    member this.height =
+        this.Grid |> Array2D.length1
+
+    member this.width =
+        this.Grid |> Array2D.length2
+
+    static member create (heightMapLines: string array) =
+        let height = heightMapLines.Length
+        let width = heightMapLines.[0].Length
+
+        let grid = Array2D.init height width (fun _ _ -> HeightPoint.init)
+
+        let getHeightPointType row col =
+            match row, col with
+            | 0, 0                                           -> TopLeftCorner
+            | 0, c when c = width-1                          -> TopRightCorner            
+            | 0, c when c > 0 && c < width-1                 -> TopMiddleEdge
+            | r, 0 when r > 0 && r < height-1                -> MiddleLeftEdge
+            | r, c when r > 0 && r < height-1 && c = width-1 -> MiddleRightEdge 
+            | r, 0 when r = height-1                         -> BottomLeftCorner
+            | r, c when r = height-1 && c > 0 && c < width-1 -> BottomMiddleEdge
+            | r, c when r = height-1 && c = width-1          -> BottomRightCorner 
+            | _                                              -> Middle
+
+        for row in [0..height-1] do
+            for col in [0..width-1] do
+                grid.[row,col] <-
+                    {
+                        Value = heightMapLines.[row].[col] |> Char.toInt |> Option.defaultValue 0
+                        Type = getHeightPointType row col
+                        Position = (row, col)
+                    }
+
+        { Grid = grid }
+
+module HeightMap =
+
+    let getSurroundingPoints (row, col) (heightMap: HeightMap) =
+        let heightPoint = heightMap.Grid.[row,col]
+        match heightPoint.Type with
+        | TopLeftCorner     -> [heightMap.Grid.[row+1,0]; heightMap.Grid.[0,col+1]]
+        | TopRightCorner    -> [heightMap.Grid.[0,col-1]; heightMap.Grid.[row+1,col]] 
+        | TopMiddleEdge     -> [heightMap.Grid.[0,col-1]; heightMap.Grid.[0,col+1]; heightMap.Grid.[row+1,col]]
+        | MiddleLeftEdge    -> [heightMap.Grid.[row-1,0]; heightMap.Grid.[row,col+1]; heightMap.Grid.[row+1,col]]
+        | MiddleRightEdge   -> [heightMap.Grid.[row-1,col]; heightMap.Grid.[row,col-1]; heightMap.Grid.[row+1,col]]
+        | BottomLeftCorner  -> [heightMap.Grid.[row-1,0]; heightMap.Grid.[row,col+1]]
+        | BottomMiddleEdge  -> [heightMap.Grid.[row,col-1]; heightMap.Grid.[row,col+1]; heightMap.Grid.[row-1,col]]
+        | BottomRightCorner -> [heightMap.Grid.[row,col-1]; heightMap.Grid.[row-1,col]]
+        | Middle            -> [heightMap.Grid.[row-1,col]; heightMap.Grid.[row+1,col]; heightMap.Grid.[row,col-1]; heightMap.Grid.[row,col+1]]
+        | Unknown           -> failwith "Unknown point type." 
+
+    let isLowPoint (row, col) (heightMap: HeightMap) =
+        let value = heightMap.Grid.[row,col].Value
+        heightMap
+        |> getSurroundingPoints (row, col)
+        |> List.forall (fun hp -> value < hp.Value)
+
+    let setPoint (row, col) n (heightMap: HeightMap) =
+        heightMap.Grid.[row,col] <- heightMap.Grid.[row,col].fill n
+
